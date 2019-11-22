25167d0a1ac289db5a86b6dd58ab4ea8551bba97
James McDonald <james@toggen.com.au>
2019-11-22 22:02:48 +1100

    latest spa build

---
+++ webroot/react/precache-manifest.2c4c9c0bcd944ed50f80e00eec66d51d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c1e5375a9dded45d203dba7e38cfba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6109ec054c418bb68206",
+    "url": "/static/css/main.a1d97170.chunk.css"
+  },
+  {
+    "revision": "70d8d9294b0fdc670f8b",
+    "url": "/static/js/2.cf41b3df.chunk.js"
+  },
+  {
+    "revision": "6109ec054c418bb68206",
+    "url": "/static/js/main.549368e9.chunk.js"
+  },
+  {
+    "revision": "46a4ef7c58845515acd8",
+    "url": "/static/js/runtime-main.ea1aca13.js"
+  }
+]);
